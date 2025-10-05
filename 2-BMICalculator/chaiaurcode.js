33883f94271dbe8e067c93f04937b9a6fbb80697
MahadevDoshetty <mahadevdoshetty317@gmail.com>
2025-10-05 10:16:38 +0000

    BMI CAlculator logic

--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -1,1 +1,30 @@
+const form = document.getElementsByTagName('form')[0];
 console.log("Welcome to Dev's Dev tools");
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const calculate = document.getElementById("calculate");
+const result = document.getElementById("results");
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validateInput();
+});
+function validateInput() {
+    let answer;
+    if (height.value.trim() && weight.value.trim()) {
+        function calculateBMI() {
+            answer = weight.value / (height.value * height.value);
+            resultValue.textContent = answer;
+        }
+        const resultValue = document.createElement('p');
+        resultValue.textContent = "Correct format";
+        resultValue.style.fontSize = "18px"
+        result.appendChild(resultValue);
+        calculateBMI();
+    }
+    else {
+        const resultValue = document.createElement('p');
+        resultValue.style.fontSize = "18px"
+        resultValue.textContent = 'Incorrect Format';
+        result.appendChild(resultValue);
+    }
+}
