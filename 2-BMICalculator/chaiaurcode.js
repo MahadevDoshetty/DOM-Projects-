bf91297901ff2dbf786733b75ed8a075c2f2529d
MahadevDoshetty <mahadevdoshetty317@gmail.com>
2025-10-06 12:40:13 +0530

    Final commit of BMI

--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -1,5 +1,5 @@
-const form = document.getElementsByTagName('form')[0];
 console.log("Welcome to Dev's Dev tools");
+const form = document.getElementsByTagName('form')[0];
 const height = document.getElementById('height');
 const weight = document.getElementById('weight');
 const calculate = document.getElementById("calculate");
@@ -17,15 +17,27 @@ function validateInput() {
             resultValue.textContent = weight.value / (height.value * height.value);
         }
         calculateBMI();
-        if (resultValue.textContent < 18.6) {
-            resultValue.style.color = "red"
-        } else if (resultValue.textContent > 24.9) {
-            resultValue.style.color = "red"
-        }else if(resultValue.textContent > 18.6 && resultValue.textContent < 24.9){
-            resultValue.style.color = "green"
-        }
+    } else {
+        calculate.addEventListener('click', () => {
+            result.textContent = "Incorrect Format";
+        });
     }
-    else {
-        resultValue.textContent = 'Incorrect Format';
+    if (resultValue.textContent < 18.6) {
+        resultValue.style.color = "red"
+    } else if (resultValue.textContent > 24.9) {
+        resultValue.style.color = "red"
+    } else if (resultValue.textContent > 18.6 && resultValue.textContent < 24.9) {
+        resultValue.style.color = "green"
     }
-}
+
+};
+height.addEventListener('click', () => {
+    result.textContent = "";
+    height.value = "";
+    weight.value = "";
+});
+weight.addEventListener('click', () => {
+    result.textContent = "";
+    height.value = "";
+    weight.value = "";
+});
