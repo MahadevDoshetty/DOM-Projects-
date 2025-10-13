936640796c001900362bc4a2c923c487e62f371d
MahadevDoshetty <mahadevdoshetty317@gmail.com>
2025-10-13 07:18:43 +0530

    Project 5 final commit

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,19 @@
-console.log('Project 5');
+console.log("Welcome to Dev's Dev Tools");
+const insert = document.getElementById("insert");
+window.addEventListener("keypress",(e)=>{
+    insert.innerHTML = `
+    <div>
+    <table>
+    <tr>
+    <td>
+    Key
+    </td>
+    <td>Key Code</td>
+    <td>Code</td>
+    </tr>
+    <td>${e.key == " " ? "Space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+    </table>
+    </div>`;
+});
